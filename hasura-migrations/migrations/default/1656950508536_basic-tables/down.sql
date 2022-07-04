fef4feddf2292c639c106b9886cff002eb99eb24
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-04 16:16:38 +0000

    Add table: board_admins

--- hasura-migrations/migrations/default/1656950508536_basic-tables/down.sql
+++ hasura-migrations/migrations/default/1656950508536_basic-tables/down.sql
@@ -1,2 +1,3 @@
 DROP TABLE "public"."boards";
 DROP TABLE "public"."users";
+DROP TABLE "public"."boards_admins";
--- hasura-migrations/migrations/default/1656950508536_basic-tables/up.sql
+++ hasura-migrations/migrations/default/1656950508536_basic-tables/up.sql
@@ -6,6 +6,12 @@ CREATE TABLE "public"."boards" (
 
 CREATE TABLE "public"."users" (
   "id" SERIAL NOT NULL, 
-  "name" TEXT NOT NULL
+  "name" TEXT NOT NULL,
   PRIMARY KEY ("id") 
+);
+
+CREATE TABLE "public"."boards_admins" (
+  "board_id" INT NOT NULL, 
+  "user_id" INT NOT NULL,
+  PRIMARY KEY ("board_id", "user_id") 
 );
