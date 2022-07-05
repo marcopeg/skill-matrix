6dac78515a24413c28c2fb46c4aba5d776546889
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-05 05:09:35 +0000

    Add partial answers table

--- hasura-migrations/migrations/default/1656950508536_basic-tables/down.sql
+++ hasura-migrations/migrations/default/1656950508536_basic-tables/down.sql
@@ -2,3 +2,4 @@ DROP TABLE "public"."boards" CASCADE;
 DROP TABLE "public"."users" CASCADE;
 DROP TABLE "public"."boards_admins" CASCADE;
 DROP TABLE "public"."questions" CASCADE;
+DROP TABLE "public"."answers" CASCADE;
--- hasura-migrations/migrations/default/1656950508536_basic-tables/up.sql
+++ hasura-migrations/migrations/default/1656950508536_basic-tables/up.sql
@@ -44,23 +44,10 @@ CREATE TABLE "public"."users" (
 CREATE TABLE "public"."boards_admins" (
   "board_id" INT NOT NULL, 
   "user_id" INT NOT NULL,
-  PRIMARY KEY ("board_id", "user_id") 
+  CONSTRAINT "board_admins_pkey" PRIMARY KEY ("board_id", "user_id"),
+  CONSTRAINT "boards_admins_board_id_fkey" FOREIGN KEY("board_id") REFERENCES "boards"("id"),
+  CONSTRAINT "boards_admins_user_id_fkey" FOREIGN KEY("user_id") REFERENCES "users"("id")
 );
-
--- Foreign Keys
-ALTER TABLE ONLY "public"."boards_admins"
-ADD CONSTRAINT "boards_admins_board_id_fkey" 
-    FOREIGN KEY ("board_id") 
-    REFERENCES "public"."boards"("id") 
-    ON UPDATE CASCADE 
-    ON DELETE CASCADE;
-
-ALTER TABLE ONLY "public"."boards_admins"
-ADD CONSTRAINT "boards_admins_user_id_fkey" 
-    FOREIGN KEY ("user_id") 
-    REFERENCES "public"."users"("id") 
-    ON UPDATE CASCADE 
-    ON DELETE CASCADE;
 
 
 
@@ -76,7 +63,8 @@ CREATE TABLE "public"."questions" (
   "data" JSON NOT NULL,
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
   "is_deleted" BOOLEAN NOT NULL DEFAULT false,
-  PRIMARY KEY ("id") 
+  CONSTRAINT "questions_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "questions_board_id_fkey" FOREIGN KEY("board_id") REFERENCES "boards"("id")
 );
 
 CREATE TRIGGER "set_public_questions_updated_at"
@@ -87,11 +75,33 @@ EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 COMMENT ON TRIGGER "set_public_questions_updated_at" ON "public"."questions" 
 IS 'Trigger to set value of column "updated_at" to current timestamp on row update';
 
--- Foreign Keys
-ALTER TABLE ONLY "public"."questions"
-ADD CONSTRAINT "questions_board_id_fkey" 
-    FOREIGN KEY ("board_id") 
-    REFERENCES "public"."boards"("id") 
-    ON UPDATE CASCADE 
-    ON DELETE CASCADE;
 
+
+
+
+---
+--- ANSWERS
+---
+
+CREATE TABLE "public"."answers" (
+  "id" BIGSERIAL NOT NULL, 
+  "board_id" INT NOT NULL,
+  "user_id" INT NOT NULL,
+  "question_id" INT NOT NULL,
+  "score" SMALLINT NOT NULL,
+  "data" JSON NOT NULL DEFAULT '{}',
+  "notes" TEXT,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  CONSTRAINT "answers_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "answers_board_id_fkey" FOREIGN KEY("board_id") REFERENCES "boards"("id"),
+  CONSTRAINT "answers_user_id_fkey" FOREIGN KEY("user_id") REFERENCES "boards"("id")
+);
+
+CREATE TRIGGER "set_public_answers_updated_at"
+BEFORE UPDATE ON "public"."answers"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+
+COMMENT ON TRIGGER "set_public_answers_updated_at" ON "public"."answers" 
+IS 'Trigger to set value of column "updated_at" to current timestamp on row update';
