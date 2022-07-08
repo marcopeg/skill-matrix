d2b62ed98dbbb6cb3826aeedd07ab37afcf530f2
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-08 04:02:41 +0000

    Add tests for the view

--- hasura-migrations/migrations/default/1656950508570_survey_questions/up.sql
+++ hasura-migrations/migrations/default/1656950508570_survey_questions/up.sql
@@ -12,15 +12,3 @@ JOIN "public"."surveys" AS "s"
   ON "q"."board_id" = "s"."board_id"
 WHERE "q"."etag" <= "s"."created_at"
 ORDER BY "s"."id" DESC, "q"."id", "q"."etag" DESC;
-
--- SELECT DISTINCT ON ("q"."id") 
---   "s"."board_id" AS "board_id", 
---   "s"."id" AS "survey_id",
---   "q"."id" AS "question_id",
---   "q"."etag",
---   "q"."type",
---   "q"."data"
--- FROM "public"."surveys" AS "s"
--- JOIN "questions" AS "q" ON "s"."board_id" = "q"."board_id"
--- WHERE "q"."is_deleted" = false
--- ORDER BY "q"."id", "q"."etag" DESC;
--- hasura-migrations/tests/default/survey.test.sql
+++ hasura-migrations/tests/default/survey.test.sql
@@ -1,15 +1,8 @@
 BEGIN;
-SELECT plan(5);
+SELECT plan(10);
 
 
-PREPARE "get_surveys_view" AS
--- SELECT 
---   "board_id",
---   "survey_id",
---   "id",
---   ("data"->>'v')::int AS "question_v"
--- FROM "public"."surveys_questions";
-
+PREPARE "get_surveys_query" AS
 SELECT DISTINCT ON ("s"."id", "q"."id")
   "s"."board_id",
   "s"."id" AS "survey_id",
@@ -22,7 +15,7 @@ WHERE "q"."etag" <= "s"."created_at"
 ORDER BY "s"."id" DESC, "q"."id", "q"."etag" DESC
 ;
 
-PREPARE "get_survey_view" (INT) AS
+PREPARE "get_survey_query" (INT) AS
 SELECT DISTINCT ON ("s"."id", "q"."id")
   "s"."board_id",
   "s"."id" AS "survey_id",
@@ -35,6 +28,23 @@ WHERE "q"."etag" <= "s"."created_at"
   AND "s"."id" = $1
 ORDER BY "s"."id" DESC, "q"."id", "q"."etag" DESC
 ;
+
+PREPARE "get_surveys_view" AS
+SELECT 
+  "board_id",
+  "survey_id",
+  "id",
+  ("data"->>'v')::int AS "question_v"
+FROM "public"."surveys_questions";
+
+PREPARE "get_survey_view" AS
+SELECT 
+  "board_id",
+  "survey_id",
+  "id",
+  ("data"->>'v')::int AS "question_v"
+FROM "public"."surveys_questions"
+WHERE "survey_id" = $1;
 
 
 
@@ -50,6 +60,12 @@ VALUES (1, 1, 'star', '{"v":1}');
 INSERT INTO "public"."surveys" ("id", "board_id") VALUES (1, 1);
 
 SELECT results_eq(
+  'get_surveys_query',
+  $$VALUES ( 1, 1, 1, 1 )$$,
+  'It should return only questions associated with a survey'
+);
+
+SELECT results_eq(
   'get_surveys_view',
   $$VALUES ( 1, 1, 1, 1 )$$,
   'It should return only questions associated with a survey'
@@ -63,6 +79,12 @@ SELECT results_eq(
 
 INSERT INTO "public"."questions" ("board_id", "id", "type", "data") 
 VALUES (1, 1, 'star', '{"v":2}');
+
+SELECT results_eq(
+  'get_surveys_query',
+  $$VALUES ( 1, 1, 1, 1 )$$,
+  'It should select the version that is closes to the survey'
+);
 
 SELECT results_eq(
   'get_surveys_view',
@@ -81,6 +103,15 @@ VALUES (1, 1, 'star', '{"v":3}'), (1, 1, 'star', '{"v":4}');
 
 INSERT INTO "public"."surveys" ("id", "board_id")
 VALUES (2, 1);
+
+SELECT results_eq(
+  'get_surveys_query',
+  $$VALUES 
+    ( 1, 2, 1, 4 )
+  , ( 1, 1, 1, 1 )
+  $$,
+  'It should select the version that is closes to the survey'
+);
 
 SELECT results_eq(
   'get_surveys_view',
@@ -104,10 +135,30 @@ INSERT INTO "public"."surveys" ("id", "board_id")
 VALUES (3, 1);
 
 SELECT results_eq(
+  'EXECUTE get_survey_query( 3 )',
+  $$VALUES 
+    ( 1, 3, 1, 4 )
+  , ( 1, 3, 2, 1 )
+  $$,
+  'It should select the version that is closes to the survey'
+);
+
+SELECT results_eq(
   'EXECUTE get_survey_view( 3 )',
   $$VALUES 
     ( 1, 3, 1, 4 )
   , ( 1, 3, 2, 1 )
+  $$,
+  'It should select the version that is closes to the survey'
+);
+
+SELECT results_eq(
+  'EXECUTE get_surveys_query',
+  $$VALUES 
+    ( 1, 3, 1, 4 )
+  , ( 1, 3, 2, 1 )
+  , ( 1, 2, 1, 4 )
+  , ( 1, 1, 1, 1 )
   $$,
   'It should select the version that is closes to the survey'
 );
