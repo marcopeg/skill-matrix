4624a96e1b1f5f3777b5533cba04e7550dfe4892
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-08 04:25:45 +0000

    Complete the test suite

--- hasura-migrations/tests/default/survey.test.sql
+++ hasura-migrations/tests/default/survey.test.sql
@@ -1,5 +1,5 @@
 BEGIN;
-SELECT plan(5);
+SELECT plan(9);
 
 PREPARE "get_surveys" AS
 SELECT 
@@ -70,7 +70,7 @@ SELECT results_eq(
     ( 1, 2, 1, 4 )
   , ( 1, 1, 1, 1 )
   $$,
-  'It should select the version that is closes to the survey'
+  'It should select the version that is closes to the survey with multiple questions available'
 );
 
 
@@ -86,12 +86,12 @@ INSERT INTO "public"."surveys" ("id", "board_id")
 VALUES (3, 1);
 
 SELECT results_eq(
-  'EXECUTE get_survey( 3 )',
+  'EXECUTE get_survey(3)',
   $$VALUES 
     ( 1, 3, 1, 4 )
   , ( 1, 3, 2, 1 )
   $$,
-  'It should select the version that is closes to the survey'
+  'It should identify multiple questions for a specific survey'
 );
 
 SELECT results_eq(
@@ -102,9 +102,83 @@ SELECT results_eq(
   , ( 1, 2, 1, 4 )
   , ( 1, 1, 1, 1 )
   $$,
-  'It should select the version that is closes to the survey'
+  'It should identify multiple questions for each survey'
 );
 
+
+
+---
+--- IT SHOULD NOT LIST DELETED QUESTIONS
+---
+
+INSERT INTO "public"."questions" ("board_id", "id", "type", "data", "is_deleted")
+VALUES (1, 2, 'star', '{"v":2}', true);
+
+INSERT INTO "public"."surveys" ("id", "board_id")
+VALUES (4, 1);
+
+SELECT results_eq(
+  'EXECUTE get_survey(4)',
+  $$VALUES ( 1, 4, 1, 4 )$$,
+  'It should skip deleted questions'
+);
+
+
+
+---
+--- IT SHOULD SKIP INTERMEDIATE VERSIONS
+---
+
+INSERT INTO "public"."questions" ("board_id", "id", "type", "data")
+VALUES (1, 3, 'star', '{"v":1}');
+
+INSERT INTO "public"."questions" ("board_id", "id", "type", "data")
+VALUES (1, 3, 'star', '{"v":2}');
+
+INSERT INTO "public"."surveys" ("id", "board_id")
+VALUES (5, 1);
+
+SELECT results_eq(
+  'EXECUTE get_survey(5)',
+  $$VALUES 
+    ( 1, 5, 1, 4 )
+  , ( 1, 5, 3, 2 )
+  $$,
+  'It should skip intermediate'
+);
+
+
+
+---
+--- IT SHOULD HANDLE MULTIPLE BOARDS
+---
+
+INSERT INTO "public"."boards" VALUES (2, 'b2');
+
+SELECT results_eq(
+  'EXECUTE get_survey(4)',
+  $$VALUES ( 1, 4, 1, 4 )$$,
+  'An existing survey should stay immutable to the creation of a new board'
+);
+
+INSERT INTO "public"."questions" ("board_id", "id", "type", "data")
+VALUES 
+  (2, 4, 'star', '{"v":1}')
+, (2, 5, 'star', '{"v":1}')
+, (2, 4, 'star', '{"v":2}')
+;
+
+INSERT INTO "public"."surveys" ("id", "board_id")
+VALUES (6, 2);
+
+SELECT results_eq(
+  'EXECUTE get_survey(6)',
+  $$VALUES 
+    ( 2, 6, 4, 2 )
+  , ( 2, 6, 5, 1 )
+  $$,
+  'It should list questions for a survey related to a new board'
+);
 
 
 SELECT * FROM finish();
