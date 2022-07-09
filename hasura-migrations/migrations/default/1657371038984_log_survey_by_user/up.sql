5a61e309f5b54e7218937b4cb81f87868edd577a
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-09 16:42:37 +0000

    Add test coverage

--- hasura-migrations/migrations/default/1657371038984_log_survey_by_user/up.sql
+++ hasura-migrations/migrations/default/1657371038984_log_survey_by_user/up.sql
@@ -12,19 +12,23 @@ DECLARE
 BEGIN
 
   -- Get question etag
-  select question.* into VAR_question
-  from (
-    select
-    ("cache" ->> 'questions')::jsonb AS "data"
-    from surveys
-    where id = (hasura_session ->> 'x-hasura-survey-id')::int
-  ) cache,
+  SELECT
+    "board_id", 
+    "question".* 
+  INTO VAR_question
+  FROM (
+    SELECT
+      "board_id",
+      ("cache" ->> 'questions')::jsonb AS "data"
+    FROM "public"."surveys"
+    WHERE "id" = ("hasura_session" ->> 'x-hasura-survey-id')::int
+  ) "cache",
   jsonb_to_recordset("cache"."data") AS "question" (
     "id" INT,
     "created_at" TIMESTAMPTZ,
     "data" JSON
   )
-  WHERE question.id = "question_id";
+  WHERE "question"."id" = "question_id";
 
   -- Check question existance
   IF VAR_question.created_at IS NULL THEN
@@ -34,25 +38,25 @@ BEGIN
   -- Append new answer version
   RETURN QUERY
   INSERT INTO "public"."answers" VALUES (
-    (hasura_session ->> 'x-hasura-board-id')::int,
-    (hasura_session ->> 'x-hasura-user-id')::int,
-    (hasura_session ->> 'x-hasura-survey-id')::int,
+    VAR_question.board_id,
+    ("hasura_session" ->> 'x-hasura-user-id')::int,
+    ("hasura_session" ->> 'x-hasura-survey-id')::int,
     question_id,
     VAR_question.created_at,
-    score,
-    data,
-    notes
+    "score",
+    "data",
+    "notes"
   ) RETURNING
-    (hasura_session ->> 'x-hasura-user-id')::int AS "user_id",
-    (hasura_session ->> 'x-hasura-board-id')::int AS "board_id",
-    (hasura_session ->> 'x-hasura-survey-id')::int AS "survey_id",
+    ("hasura_session" ->> 'x-hasura-user-id')::int AS "user_id",
+    VAR_question.board_id,
+    ("hasura_session" ->> 'x-hasura-survey-id')::int AS "survey_id",
     question_id AS "question_id",
     VAR_question.created_at AS "question_created_at",
     VAR_question.data::json AS "question_data",
     created_at AS "answer_created_at",
-    score AS "answer_score",
-    data AS "answer_data",
-    notes AS "answer_notes"
+    "score" AS "answer_score",
+    "data" AS "answer_data",
+    "notes" AS "answer_notes"
   ;
 END;
 $$ VOLATILE LANGUAGE plpgsql;
---
+++ hasura-migrations/tests/default/survey-log-answer.test.sql
@@ -0,0 +1,51 @@
+BEGIN;
+SELECT plan(1);
+
+INSERT INTO "public"."boards" VALUES (1, 'b1');
+
+INSERT INTO "public"."questions" 
+  ("board_id",  "id",   "data",     "created_at") VALUES 
+  (1,           1,      '{"v":1}',  '2022-07-08 11:10')
+, (1,           2,      '{"v":1}',  '2022-07-08 11:10')
+;
+
+INSERT INTO "public"."surveys" 
+  ("id",  "board_id", "created_at") VALUES 
+  (1,     1,          '2022-07-08 11:10');
+
+INSERT INTO "public"."users" 
+  ("id",  "name") VALUES 
+  (1,     'User1')
+, (2,     'User2')
+;
+
+INSERT INTO "public"."surveys_invites" 
+  ("survey_id",   "user_id") VALUES 
+  (1,             1)
+, (1,             2)
+;
+
+SELECT * FROM "public"."log_survey_by_user"(
+  '{
+    "x-hasura-user-id": 1,
+    "x-hasura-survey-id": 1
+  }'::json,
+  1,
+  10::smallint
+);
+
+SELECT results_eq(
+  $$SELECT
+      "question_id",
+      "answer_score"::text
+    FROM "public"."get_survey_by_user"('{"x-hasura-survey-id":1,"x-hasura-user-id":1}'::json)$$,
+  $$VALUES 
+    ( 1, '10' )
+  , ( 2, null)
+  $$,
+  'It should join a survey cached questions with the related available answers'
+);
+
+
+SELECT * FROM finish();
+ROLLBACK;
