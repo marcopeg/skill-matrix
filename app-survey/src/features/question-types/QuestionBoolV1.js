d88774fcc439f7e1829d44b0e4c330c825bc5b16
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-10 10:52:09 +0000

    Improve basic questions UI

--- app-survey/src/features/question-types/QuestionBoolV1.js
+++ app-survey/src/features/question-types/QuestionBoolV1.js
@@ -1,24 +1,63 @@
 import { useState } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import FormHelperText from "@mui/material/FormHelperText";
 
-export const QuestionBoolV1 = ({ question, onConfirm }) => {
+const noop = () => {};
+
+export const QuestionBoolV1 = ({
+  question,
+  onConfirm = noop,
+  onChange = noop
+}) => {
   const { title } = question.schema;
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState(question.score);
+  const [isConfirmed, setIsConfirmed] = useState(question.score !== null);
 
-  const apply = (nextValue) => {
+  const apply = (evt) => {
+    if (evt.target.value === undefined) return;
+    const nextValue = parseInt(evt.target.value, 10);
+
+    setIsConfirmed(value === nextValue);
     if (value === nextValue) {
       onConfirm(value);
     } else {
       setValue(nextValue);
+      onChange(nextValue);
     }
   };
 
   return (
-    <div>
-      <h6>{title}</h6>
-      <div onClick={() => apply(true)}>true {value === true ? "*" : null}</div>
-      <div onClick={() => apply(false)}>
-        false {value === false ? "*" : null}
-      </div>
-    </div>
+    <FormControl>
+      <FormLabel>{title}</FormLabel>
+      <RadioGroup row value={value} onClick={apply}>
+        <FormControlLabel
+          value="100"
+          control={
+            <Radio
+              {...(isConfirmed ? { color: "success" } : { color: "primary" })}
+            />
+          }
+          label="True"
+        />
+        <FormControlLabel
+          value="0"
+          control={
+            <Radio
+              {...(isConfirmed ? { color: "success" } : { color: "primary" })}
+            />
+          }
+          label="False"
+        />
+      </RadioGroup>
+      {value !== null && !isConfirmed && (
+        <FormHelperText>
+          Click again on the star to confirm your choice!
+        </FormHelperText>
+      )}
+    </FormControl>
   );
 };
--- app-survey/src/features/survey-page-view/SurveyPageView.js
+++ app-survey/src/features/survey-page-view/SurveyPageView.js
@@ -1,5 +1,6 @@
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
 
 export const SurveyPageView = ({ questions, renderQuestion }) => {
   // Here some hooks should provide the division by page
@@ -8,11 +9,14 @@ export const SurveyPageView = ({ questions, renderQuestion }) => {
   return (
     <>
       <Stack divider={<Divider orientation="horizontal" flexItem />}>
-        {questions.map(($) =>
-          renderQuestion($, {
-            onConfirm: (e) => console.log("confirm value", e)
-          })
-        )}
+        {questions.map((question) => (
+          <Box key={question.id} sx={{ mt: 5, mb: 5 }}>
+            {renderQuestion(question, {
+              onConfirm: (e) => console.log("confirm value", e),
+              onChange: (e) => console.log("change value", e)
+            })}
+          </Box>
+        ))}
       </Stack>
     </>
   );
