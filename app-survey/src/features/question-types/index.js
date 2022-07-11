97852b2aee832d210bda7c4af627d2981c2c408a
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-11 12:28:25 +0000

    Add multiple choiche question

--- app-survey/src/features/question-types/index.js
+++ app-survey/src/features/question-types/index.js
@@ -1,6 +1,7 @@
 import { QuestionStarV1 } from "./QuestionStarV1";
 import { QuestionBoolV1 } from "./QuestionBoolV1";
 import { QuestionScaleV1 } from "./QuestionScaleV1";
+import { QuestionMultiV1 } from "./QuestionMultiV1";
 
 export const questionTypes = () => [
   {
@@ -22,6 +23,13 @@ export const questionTypes = () => [
     handler: {
       type: "question:scale@1",
       component: QuestionScaleV1
+    }
+  },
+  {
+    target: "$SURVEY_QUESTION_TYPE",
+    handler: {
+      type: "question:multi@1",
+      component: QuestionMultiV1
     }
   }
 ];
