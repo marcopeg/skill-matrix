95e5e31baf60d9c4bf7393aa783b021496cd11b9
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-11 13:25:39 +0000

    Implement inner-scroll for Page's body

--- app-survey/src/features/survey-page-view/SurveyPageView.js
+++ app-survey/src/features/survey-page-view/SurveyPageView.js
@@ -9,58 +9,56 @@ export const SurveyPageView = (props) => {
     useSurveyPageView(props);
 
   return (
-    <>
-      <Stack divider={<Divider orientation="horizontal" flexItem />}>
-        {questions.map((question) => (
+    <Stack divider={<Divider orientation="horizontal" flexItem />}>
+      {questions.map((question) => (
+        <Box
+          key={question.id}
+          sx={{
+            transition: "all 0.3s ease-in-out",
+            borderLeft: "10px solid white",
+            ...(question.isActive
+              ? {
+                  borderColor: "primary.main"
+                }
+              : question.hasAnswer
+              ? {
+                  borderColor: "success.main"
+                }
+              : {
+                  borderColor: "inherith"
+                })
+          }}
+        >
           <Box
-            key={question.id}
+            id={`question-${question.id}`}
             sx={{
-              transition: "all 0.3s ease-in-out",
-              borderLeft: "10px solid white",
-              ...(question.isActive
-                ? {
-                    borderColor: "primary.main"
-                  }
-                : question.hasAnswer
-                ? {
-                    borderColor: "success.main"
-                  }
-                : {
-                    borderColor: "inherith"
-                  })
+              pt: 15,
+              pb: 15,
+              pl: 2,
+              pr: 2,
+              transform: question.isActive ? "scale(1)" : "scale(0.8)",
+              transition: "transform 0.3s ease-in-out",
+              scrollMargin: "10vh"
             }}
           >
-            <Box
-              id={`question-${question.id}`}
-              sx={{
-                pt: 15,
-                pb: 15,
-                pl: 2,
-                pr: 2,
-                transform: question.isActive ? "scale(1)" : "scale(0.8)",
-                transition: "transform 0.3s ease-in-out",
-                scrollMargin: "10vh"
-              }}
-            >
-              {renderQuestion(question, {
-                onConfirm: (score, data = {}, notes = "") => {
-                  // Move to next question:
-                  scrollToActiveQuestion();
+            {renderQuestion(question, {
+              onConfirm: (score, data = {}, notes = "") => {
+                // Move to next question:
+                scrollToActiveQuestion();
 
-                  // Submit the answer's value:
-                  logAnswer({
-                    questionId: question.id,
-                    score,
-                    notes,
-                    data
-                  });
-                }
-                // onChange: (e) => console.log("change value", e)
-              })}
-            </Box>
+                // Submit the answer's value:
+                logAnswer({
+                  questionId: question.id,
+                  score,
+                  notes,
+                  data
+                });
+              }
+              // onChange: (e) => console.log("change value", e)
+            })}
           </Box>
-        ))}
-      </Stack>
-    </>
+        </Box>
+      ))}
+    </Stack>
   );
 };
