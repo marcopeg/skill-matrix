564a5b24aa07efb8d5197aed6c9fbcf0396b2287
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-11 07:34:23 +0000

    Add scroll-to-qestion

--- app-survey/src/features/survey-page-view/SurveyPageView.js
+++ app-survey/src/features/survey-page-view/SurveyPageView.js
@@ -2,25 +2,34 @@ import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import Box from "@mui/material/Box";
 
-export const SurveyPageView = ({ questions, renderQuestion, logAnswer }) => {
-  // Here some hooks should provide the division by page
-  // then current page and current question...
+import { useSurveyPageView } from "./use-survey-page-view";
+
+export const SurveyPageView = (props) => {
+  const { questions, renderQuestion, logAnswer, scrollToActiveQuestion } =
+    useSurveyPageView(props);
 
   return (
     <>
       <Stack divider={<Divider orientation="horizontal" flexItem />}>
         {questions.map((question) => (
-          <Box key={question.id} sx={{ mt: 5, mb: 5 }}>
-            {renderQuestion(question, {
-              onConfirm: (score, data = {}, notes = "") =>
-                logAnswer({
-                  questionId: question.id,
-                  score,
-                  notes,
-                  data
-                })
-              // onChange: (e) => console.log("change value", e)
-            })}
+          <Box key={question.id} sx={{ mt: 10, mb: 10 }}>
+            <Box id={`question-${question.id}`} sx={{ pt: 10, pb: 10 }}>
+              {renderQuestion(question, {
+                onConfirm: (score, data = {}, notes = "") => {
+                  // Move to next question:
+                  scrollToActiveQuestion();
+
+                  // Submit the answer's value:
+                  logAnswer({
+                    questionId: question.id,
+                    score,
+                    notes,
+                    data
+                  });
+                }
+                // onChange: (e) => console.log("change value", e)
+              })}
+            </Box>
           </Box>
         ))}
       </Stack>
