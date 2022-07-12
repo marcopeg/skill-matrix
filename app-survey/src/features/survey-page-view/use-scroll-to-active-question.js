15fb538e4a5213da230dda9ec73d4d8eeed9477b
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-07-12 06:52:10 +0000

    Improve memoization

--- app-survey/src/features/survey-page-view/use-scroll-to-active-question.js
+++ app-survey/src/features/survey-page-view/use-scroll-to-active-question.js
@@ -1,9 +1,10 @@
 // https://stackoverflow.com/questions/43441856/how-to-scroll-to-an-element#:~:text=React%2016.8%20%2B%2C%20Functional%20component
 
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 
 export const useScrollToActiveQuestion = (activeQuestion) => {
-  const scrollToActiveQuestion = () => {
+  const scrollToActiveQuestion = useCallback(() => {
+    console.log("@scroll");
     if (!activeQuestion) return;
 
     const el = document.getElementById(`question-${activeQuestion.id}`);
@@ -12,12 +13,12 @@ export const useScrollToActiveQuestion = (activeQuestion) => {
       block: "start",
       inline: "nearest"
     });
-  };
+  }, [activeQuestion]);
 
   // Scroll to the next question that needs an answer:
   useEffect(() => {
     scrollToActiveQuestion();
-  });
+  }, [activeQuestion, scrollToActiveQuestion]);
 
   return {
     scrollToActiveQuestion
